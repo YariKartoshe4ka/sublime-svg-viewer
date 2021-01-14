52d2679b5d52486aa3831bc483ecdde083437793
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-01-14 16:37:27 +0300

    Добавление экспорта настроек и возможности измениить SVG конвертер

--- svg-viewer.py
+++ svg-viewer.py
@@ -6,29 +6,48 @@ from hashlib import sha256
 from shutil import which
 
 
+
+# Sublime plugin events functions
 def plugin_loaded():
-    SvgViewerCommand.settings = sublime.load_settings('svg-viewer.sublime-settings')
+    SvgViewerViewSvgCommand.settings = sublime.load_settings('svg-viewer.sublime-settings')
+
+def plugin_unloaded():
+    sublime.save_settings('svg-viewer.sublime-settings')
 
 
-class SvgViewerCommand(sublime_plugin.TextCommand):
+
+# Funcions for work with converters
+def get_index_by_converter(converter):
+    return list(SvgViewerViewSvgCommand.converters.keys()).index(converter)
+
+def get_converter_by_index(index):
+    return list(SvgViewerViewSvgCommand.converters.keys())[index]
+
+def get_converters():
+    return list(SvgViewerViewSvgCommand.converters.keys())
+
+
+# Commands
+class SvgViewerViewSvgCommand(sublime_plugin.TextCommand):
     tmp_dir = os.path.join(gettempdir(), 'svg-viewer')
 
     converters = {
-        'cairosvg': 'cairosvg {} -o {} -d {}'
+        'inkspace': 'inkspace --export-type=png "{name}" -o "{out}"',
+        'cairosvg': 'cairosvg "{name}" -o "{out}" -d {dpi}'
     }
 
     def run(self, edit):
         converter = self.settings.get('converter', '')
 
         if not converter or not which(converter) or converter not in self.converters.keys():
-            self.view.window().show_quick_panel(list(self.converters.keys()), self.convert)
+            self.view.window().show_quick_panel(get_converters(), self.convert)
         else:
-            self.convert(list(self.converters.keys()).index(converter))
+            self.convert(get_index_by_converter(converter))
 
 
     def convert(self, index):
         if index >= 0:
-            self.settings.set('converter', list(self.converters.keys())[index])
+            self.settings.set('converter', get_converter_by_index(index))
 
             if not os.path.exists(self.tmp_dir):
                 os.mkdir(self.tmp_dir)
@@ -41,17 +60,20 @@ class SvgViewerCommand(sublime_plugin.TextCommand):
             out = os.path.join(self.tmp_dir, out)
 
             if os.path.exists(out):
-                self.view.window().open_file(out, flags=sublime.TRANSIENT)
+                self.view.window().open_file(out, flags=sublime.TRANSIENT if self.settings.get('open_picture_in_preview_mode') else 0)
             else:
-                cmd = self.converters[list(self.converters.keys())[index]].format(name, out, dpi)
-                print(cmd)
+                cmd = self.converters[get_converter_by_index(index)].format(name=name, out=out, dpi=dpi)
 
-                p = os.popen(cmd)
-                exitcode = p.close()
+                os.popen(cmd)
 
-                if exitcode:
-                    raise Exception('File not SVG')
+            self.view.window().open_file(out, flags=sublime.TRANSIENT if self.settings.get('open_picture_in_preview_mode') else 0)
 
-            self.view.window().open_file(out, flags=sublime.TRANSIENT)
+
+class SvgViewerChangeConverter(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.window().show_quick_panel(get_converters(), self.change)
+
+    def change(self, index):
+        SvgViewerViewSvgCommand.settings.set('converter', get_converter_by_index(index))
 
 
